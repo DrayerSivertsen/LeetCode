e2ccc7cd4e032ab55d4b01a02570d28d78746a54
Drayer Sivertsen <drayer.sivertsen@wsu.edu>
2023-02-08 00:16:53 -0800

    leetcode 287

---
+++ linked_list/fast_slow_ptr/287.py
@@ -0,0 +1,27 @@
+"""
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+There is only one repeated number in nums, return this repeated number.
+
+You must solve the problem without modifying the array nums and uses only constant extra space.
+"""
+
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow, fast = 0, 0
+
+        # find cycle
+        while True: # do while loop
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+
+        # find head of cycle
+        slow2 = 0
+        while True: # do while loop
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if slow == slow2:
+                return slow
