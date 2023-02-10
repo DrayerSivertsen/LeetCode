5aeeba4913bdfb4ec90916526d8fa99d680dba14
Drayer Sivertsen <drayer.sivertsen@wsu.edu>
2023-02-10 00:16:50 -0800

    new

---
+++ heaps/two_heaps/295.py
@@ -0,0 +1,46 @@
+"""
+The median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value, and the median is the mean of the two middle values.
+
+For example, for arr = [2,3,4], the median is 3.
+For example, for arr = [2,3], the median is (2 + 3) / 2 = 2.5.
+Implement the MedianFinder class:
+
+MedianFinder() initializes the MedianFinder object.
+void addNum(int num) adds the integer num from the data stream to the data structure.
+double findMedian() returns the median of all elements so far. Answers within 10-5 of the actual answer will be accepted.
+"""
+
+
+import heapq
+class MedianFinder:
+    def __init__(self):
+        self.small, self.large = [], []
+
+    def addNum(self, num: int) -> None:
+        # push to maxheap and swap with minheap if needed
+        heapq.heappush(self.small, -1 * num)
+
+        # maxheap val greater than minheap val
+        if (self.small and self.large and (-1 * self.small[0]) > self.large[0]):
+            val = -1 * heapq.heappop(self.small) 
+            heapq.heappush(self.large, val)
+
+        # handle uneven size
+        if len(self.small) > len(self.large) + 1:
+            val = -1 * heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+        if len(self.large) > len(self.small) + 1:
+            val = heapq.heappop(self.large)
+            heapq.heappush(self.small, -1 * val)
+
+    def findMedian(self) -> float:
+        if len(self.small) > len(self.large): # median in maxheap
+            return -1 * self.small[0]
+        elif len(self.large) > len(self.small): # median in minheap
+            return self.large[0]
+        return (-1 * self.small[0] + self.large[0]) / 2 # median average of both
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
