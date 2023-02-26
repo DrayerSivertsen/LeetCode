a4b9efdf1c0fe900686ea12936c1058759dd053f
Drayer Sivertsen <drayer.sivertsen@wsu.edu>
2023-02-25 20:07:46 -0800

    leetcode 77

--- backtracking/combinations/77.py
+++ backtracking/combinations/77.py
@@ -9,20 +9,15 @@ class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         res = []
 
-        def helper(i: int, comb: List[int]):
-            if len(comb) == k: # combination length of k
+        def helper(start: int, comb: List[int]):
+            if len(comb) == k: # combination of length k
                 res.append(comb.copy())
                 return
             
-            if i > n: # always empty combination
-                return
-            
-            comb.append(i)
-            helper(i+1, comb) # dfs adding i
-            comb.pop()
-
-            helper(i+1, comb) # dfs not adding i
+            for i in range(start, n + 1):
+                comb.append(i)
+                helper(i + 1, comb) # one less value to loop through every call
+                comb.pop()
         
         helper(1, [])
-
         return res
