f5145db1c4c11fe50bd11f31f230d99ca8c8e75a
Drayer Sivertsen <drayer.sivertsen@wsu.edu>
2023-03-25 22:44:16 -0700

    leetcode 207

---
+++ dynamic/416.py
@@ -0,0 +1,63 @@
+"""
+Given an integer array nums, return true if you can partition the array into two subsets such that the sum of the elements in both subsets is equal or false otherwise.
+"""
+
+
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        if sum(nums) % 2:
+            return False
+
+        target = sum(nums) / 2
+
+        dp = set()
+        dp.add(0)
+
+        for i in range(len(nums)):
+            nextDP = set()
+            for t in dp:
+                nextDP.add(t + nums[i])
+                nextDP.add(t)
+            dp = nextDP
+        return True if target in dp else False
+
+
+
+        
+class Solution:
+    def canPartition(self, nums: list[int]) -> bool:
+        if sum(nums) % 2:
+            return False
+
+        target = sum(nums) / 2
+
+        def dfsHelper(i, arr, nums):
+            print(arr, nums)
+            if target == sum(nums):
+                print('here')
+                return True
+
+            if i >= len(nums):
+                return False
+
+            if dfsHelper(i+1, arr.copy(), nums.copy()):
+                return True
+
+            tmp = nums[i]
+            nums[i] = 0
+            arr.append(tmp)
+             # append curr to arr
+
+
+            return dfsHelper(i+1, arr.copy(), nums.copy())
+            
+        return dfsHelper(0, [], nums)
+
+
+s = Solution()
+
+print(s.canPartition([1,5,11,5]))
+        
+
+                
+
