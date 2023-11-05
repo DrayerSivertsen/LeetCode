393e96b03ac8fc5eb6bb0c09ad2440a9014d41b1
DrayerSivertsen <drayer.sivertsen@wsu.edu>
2023-11-04 18:03:51 -0700

    added my solution

--- arrays/misc/55.py
+++ arrays/misc/55.py
@@ -2,6 +2,23 @@
 
 """
 
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        i, end = 0, len(nums) - 1
+        limit = nums[i]
+
+        # edge case: nums len of 1
+        if len(nums) == 1:
+            return True
+
+        while i <= limit:
+            if i + nums[i] >= end:
+                return True
+
+            limit = max(limit, i + nums[i])
+            i += 1
+
+        return False
 
 
 class Solution:
