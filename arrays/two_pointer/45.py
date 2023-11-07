e797c92b122875a378e34d64866b5d3eadd54ca7
DrayerSivertsen <drayer.sivertsen@wsu.edu>
2023-11-06 20:16:15 -0800

    leetcode 45

---
+++ arrays/two_pointer/45.py
@@ -0,0 +1,23 @@
+"""
+You are given a 0-indexed array of integers nums of length n. You are initially positioned at nums[0].
+
+Each element nums[i] represents the maximum length of a forward jump from index i. In other words, if you are at nums[i], you can jump to any nums[i + j]
+"""
+
+# count sequential windows where jumps of shortest and farthest in the window are incremented
+
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        res = 0
+        l, r = 0, 0
+
+        while r < len(nums) - 1:
+            farthest = 0
+            for i in range(l, r + 1):
+                farthest = max(farthest, i + nums[i])
+            l = r + 1
+            r = farthest
+            res += 1
+
+        return res
