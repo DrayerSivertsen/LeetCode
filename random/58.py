b1ddda8e573b276441b922a2b05796fd9d653a13
DrayerSivertsen <drayer.sivertsen@wsu.edu>
2023-11-16 22:19:00 -0800

    leetcode 42

--- random/58.py
+++ random/58.py
@@ -9,6 +9,25 @@ substring
 
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
+        res = 0
+        i = len(s) - 1
+
+        # skip spaces
+        while s[i] == ' ':
+            i -= 1
+
+        # count word
+        while i >= 0 and s[i] != ' ':
+            res += 1
+            i -= 1
+
+        return res
+            
+
+
+# correct solution but doesn't use algorithm
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
         s = s.strip().split(' ') # split words
 
         return len(s[-1]) # return last word
