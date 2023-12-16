858d84cb52380421d8c6f15fdc176ef76d6460d4
DrayerSivertsen <drayer.sivertsen@wsu.edu>
2023-12-16 15:23:56 -0800

    updated

--- arrays/two_pointer/125.py
+++ arrays/two_pointer/125.py
@@ -7,21 +7,24 @@ Given a string s, return true if it is a palindrome, or false otherwise.
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        L, R = 0, len(s) - 1
+        l, r = 0, len(s) - 1
 
-        while L < R:
-            while L < R and not self.alphaNum(s[L]): # move until alphaNum
-                L += 1
-            while R > L and not self.alphaNum(s[R]): # move until alphaNum
-                R -= 1
-
-            if s[L].lower() != s[R].lower(): # check palindrome
+        while l < r:
+            while l < r and not self.alphaNum(s[l]):
+                l += 1
+            while l < r and not self.alphaNum(s[r]):
+                r -= 1
+            
+            if s[l].lower() != s[r].lower():
                 return False
-            L, R = L + 1, R - 1
+
+            l += 1
+            r -= 1
 
         return True
 
-    def alphaNum(self, c): # checks if char is letter or number
-        return (ord('A') <= ord(c) <= ord('Z') or 
-                ord('a') <= ord(c) <= ord('z') or 
+
+    def alphaNum(self, c):
+        return (ord('A') <= ord(c) <= ord('Z') or
+                ord('a') <= ord(c) <= ord('z') or
                 ord('0') <= ord(c) <= ord('9'))
