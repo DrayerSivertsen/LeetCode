f4777bc4dd7769bd896e2d665fe576f55dcf56d2
DrayerSivertsen <drayer.sivertsen@wsu.edu>
2024-01-04 21:47:59 -0800

    leetcode 46

--- backtracking/permutations/46.py
+++ backtracking/permutations/46.py
@@ -5,18 +5,20 @@ Given an array nums of distinct integers, return all the possible permutations. 
 
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        # O(n^2 * n!)
-        def helper(i, nums):   
+        def helper(i, nums):
+            res = []
+
+            # base case
             if i == len(nums):
                 return [[]]
-            
-            res = []
+
             perms = helper(i + 1, nums)
             for p in perms:
                 for j in range(len(p) + 1):
                     pCopy = p.copy()
                     pCopy.insert(j, nums[i])
                     res.append(pCopy)
+
             return res
 
         return helper(0, nums)
